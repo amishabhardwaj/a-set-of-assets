a595be3575d0b4adcebc8fc6d6d7e4731d4a4f67
KshitijV97 <writetokshitij@gmail.com>
2020-10-10 21:29:44 +0530

    Fetched current user details on login and stored in currentUser session Attribute, Added lastLogin update too

--- src/com/hsbc/controller/LoginServlet.java
+++ src/com/hsbc/controller/LoginServlet.java
@@ -8,18 +8,26 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.hsbc.dao.AuthenticationDao;
+import com.hsbc.dao.UserDao;
+import com.hsbc.models.User;
 
 public class LoginServlet extends HttpServlet {
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		int userid = Integer.parseInt(request.getParameter("userid"));
 		String password = request.getParameter("password");
 		AuthenticationDao authDao = new AuthenticationDao();
+		UserDao userDao = new UserDao();
 		/*
 		The function verifyCredentials(userid, password) is written assuming that USERNAME and PASSWORD are stored in USERS table, If not then move the verifyCredentials(username, password) function from UserBeanDao to 
 		 */
 		if(authDao.verifyCredentials(userid, password)) {
+			User currentUser = userDao.fetchUserDetails(userid);
+			boolean lastLoginUpdated = userDao.updateLastLogin(userid);
+			
 			HttpSession session = request.getSession();
 			session.setAttribute("userid", userid);
+			session.setAttribute("currentUser",currentUser);
+			
 			response.sendRedirect("borrowAssets.jsp");
 		} else {
 			response.sendRedirect("login.jsp");
